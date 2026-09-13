de267fefb4659c83c7bbf25e83ccdfc6dc3707ad
penguinwokrs <dev.and.penguin@gmail.com>
2026-09-14 00:37:02 +0900

    Name the model ids instead of spelling them as numbers

--- tests/OpenInzone.Core.Tests/Model/ModelInfoTests.cs
+++ tests/OpenInzone.Core.Tests/Model/ModelInfoTests.cs
@@ -9,10 +9,10 @@ public class ModelInfoTests
 {
     /// <summary>INZONE Hub offers the microphone mute button on INZONE Buds and nothing else (#19).</summary>
     [Theory]
-    [InlineData(4, true)]   // INZONE Buds
-    [InlineData(0, false)]  // INZONE H9
-    [InlineData(3, false)]  // INZONE H5
-    [InlineData(5, false)]  // INZONE H9 II
+    [InlineData(ModelInfo.InzoneBuds, true)]
+    [InlineData(ModelInfo.InzoneH9, false)]
+    [InlineData(ModelInfo.InzoneH5, false)]
+    [InlineData(ModelInfo.InzoneH9II, false)]
     [InlineData(99, false)] // a model this build does not know
     public void Only_the_earbuds_take_a_microphone_mute_from_the_computer(byte modelId, bool accepts)
     {
